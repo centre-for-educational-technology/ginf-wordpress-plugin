c3fdf8bc35c3a1b40f09ba55f9e464da80c15b75
Pjotr Savitski <pjotr.savitski@gmail.com>
2020-11-12 18:11:03 +0200

    Changed code that automatically opens the side menu
    It seems that structure of HTML code has changed and new selector was required.

--- public/js/pressbooks.js
+++ public/js/pressbooks.js
@@ -3,9 +3,9 @@
 
    $(function() {
 		 // Make sure that book side menu is always open, provided one is present
-		 if ($('.reading-header__toc.dropdown > h3').length > 0) {
+		 if ($('.reading-header__toc.dropdown > .reading-header__toc__title').length > 0) {
 			 var intervalId = setInterval(function() {
-				 var button = $('.reading-header__toc.dropdown > h3 > button');
+				 var button = $('.reading-header__toc.dropdown > .reading-header__toc__title > button');
 				 if (button.length > 0) {
 					 clearInterval(intervalId);
 					 button.trigger('click');
